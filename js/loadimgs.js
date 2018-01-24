52742871030cbb083be01ce73ac86537489996c4
OlegSidor <mrx27898@gmail.com>
2018-01-24 16:36:12 +0200

    up

--- js/loadimgs.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() { //при запуску сторінки відправляємо ніформацію на сервер
-  $.post("php/loadimgs.php" , function(data){
-    var img = JSON.parse(data);
-    for (var i = 1; i < img.length; i++) { // добавляємо всі картинки на сторінку
-      $("div#content.row").append("<div class='col-xs-6 col-md-4'><a href='#' class='thumbnail'><img src='works/"+img[i]+"' alt='...'></a></div>")
-    }
-
-  });
-})
--- php/loadimgs.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_POST)){ // провіряємо чи є хотяби якісь данні
-  $img = scandir("../works");
-    echo json_encode(array_slice($img,1)); // відправляємо масив з усіма файлами у папці
-  }
- ?>
